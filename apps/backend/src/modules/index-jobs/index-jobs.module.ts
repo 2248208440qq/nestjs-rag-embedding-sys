55ccf1105b44a669a783da5f52ca16ad267be2b3
李泉 <12462229+li-quan369@user.noreply.gitee.com>
2026-06-29 21:22:34 +0800

    feat(index-jobs): 添加 AppConfigModule 依赖

--- apps/backend/src/modules/index-jobs/index-jobs.module.ts
+++ apps/backend/src/modules/index-jobs/index-jobs.module.ts
@@ -2,6 +2,7 @@ import { BullModule } from '@nestjs/bullmq';
 import { Module } from '@nestjs/common';
 
 import { INDEX_JOBS_QUEUE } from '@/common/constants';
+import { AppConfigModule } from '@/config/config.module';
 import { PrismaModule } from '@/prisma/prisma.module';
 import { QueueModule } from '@/queue/queue.module';
 import { ChunkingModule } from '@/modules/chunking/chunking.module';
@@ -15,6 +16,7 @@ import { IndexJobsService } from '@/modules/index-jobs/index-jobs.service';
 
 @Module({
   imports: [
+    AppConfigModule,
     PrismaModule,
     QueueModule,
     ChunkingModule,
